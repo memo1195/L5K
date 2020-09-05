9e1487d10984507d056fcde0e9b9d3239adba58a
memo1195 <memitoduarte.o@gmail.com>
2020-09-05 14:16:09 -0700

    created a Regular Expression to find tags in Rungs and identify the name of the action being used. Created methods and added comments for future developemtn in the algorithm for this action.

--- Rung.cs
+++ Rung.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace L5K
 {
@@ -7,6 +8,12 @@ namespace L5K
         private List<string> _comment;
         private List<string> _content;
         private List<Alarm> _alarmList;
+        private HashSet<Tag> _tags;
+        private List<string> _specialcasesList;//This is a list of Studio5000 Methods that dont necesarily implement a Tag
+        private Regex _regex;
+        private readonly string _action;
+        private readonly string _parameters;
+
         public static string RCInit
         {
             get
@@ -52,6 +59,13 @@ namespace L5K
             //identifying if there is a rung comment or not and separate them.            
             _content = new List<string>();
             _comment = new List<string>();
+            _alarmList = new List<Alarm>();
+            _specialcasesList = new List<string>() { "GSV", "SSV" };
+            _regex = new Regex(@"(?<action>[A-Z]{3})(?<parenthesis>\((?<params>[\w.,\[\]\?\+\-\#]*)\))");
+            _action = "action";
+            _parameters = "params";
+            //This regex will be used to find functions and their parenthesis content
+
             HasComment = content.Contains(RCInit);
             var viewingComment = HasComment;
             //If the code finds an RC: then it will add the content to _comment List, else it will add it will identify
@@ -71,14 +85,32 @@ namespace L5K
 
             //Content = content;
         }
+        private void DefineTags()
+        {
+            var testableString = "";//This will carry all the  content of N: and be tested in a Regex
+            foreach(var line in _content)//Gets all the content and makes it a single line string;
+            {
+                testableString += line;
+            }
+            var matches=_regex.Matches(testableString);
+            foreach(Match match in matches)
+            {
+                if (_specialcasesList.Contains(match.Groups[_action].Value)){
+                    //This will contain the algorithm to retrieve the Tags in special cases 09/05/2020
+                }
+                else
+                {
+                    //var params=match.Groups[_parameters].Value;
+                    //This needs an algorithm to separate parameters making a substring from commas, dots and '[' to get only the main tag name
+                }
+            }
+        }
 
         private void FindAlarms()
         {
             //This is an algorithm that checks the content of the RC and identifies the number of alamrs it has,
             //gets them in an array with their Index number and adds them to a List of Alarms
             var numberofAlarms = _comment.FindAllIndex(x => x.StartsWith(Alarm.AlarmInit)).ToArray();
-            if (numberofAlarms.Length > 0)
-                _alarmList = new List<Alarm>();
             foreach (var alarmIndex in numberofAlarms)
             {
                 _alarmList.Add(new Alarm(_comment[alarmIndex]));
